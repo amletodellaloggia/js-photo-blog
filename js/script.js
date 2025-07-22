83ec74ac8813769ece32e80b502a5966f81c1d34
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-22 10:35:22 +0200

    [upd] readme.md (new milestones), script.js (added date paragraph in card, added ids to card tags), style.css (updated style, defined style by tag)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Inizio facendo una chiamata usando Axios verso l'endpoint dell'API che ci fornisce le foto
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
   const data = response.data; // Alla risposta prendo i dati del campo .data, che è un array di oggetti e ognuno rappresenta un'immagine da inserire nelle card
+
   const container = document.getElementById("pictures-container"); // Punto al container del DOM dove voglio inserire le immagini
 
   // Creo dinamicamente una colonna (mi servo di Bootstrap) da appendere alla riga dell'HTML -PER OGNI- elemento dell'array
@@ -10,18 +11,20 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
 
     // Inserisco all’interno della colonna una card
     // Nell’immagine metto l’URL fornito dall’API, il testo sarà recuperato sempre dall'API sfruttando title
+    // Aggiungo direttamente in HTML la card 
     col.innerHTML = `
   <div class="card h-100" style="position: relative;">
     <img src="${picture.url}" class="card-img-top" alt="${
-      picture.title || "Foto"
-    }">
+      picture.title 
+    }"> 
     <img src="/img/pin.svg" class="pin-img-top" alt="pin">
     <div class="card-body">
-      <p class="card-text">${picture.title || ""}</p>
+      <p class="card-text" id="picture-date">${picture.date || ""}</p>
+      <p class="card-text fw-bold text-uppercase" id="picture-title">${picture.title || ""}</p>
     </div>
   </div>
 `;
-    // Infine aggiungo al contenitore la colonna, così viene visualizzata in pagina
+    // Infine aggiungo la colonna al contenitore, così viene visualizzata in pagina
     container.appendChild(col);
   });
 });
