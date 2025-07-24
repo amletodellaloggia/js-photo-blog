f86e62959150fc7e5cf5e8ebc99244cca6faefa6
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-24 13:49:07 +0200

    [upd] index.html, script.js (formatting, comments)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// CARDS
 // Inizio facendo una chiamata usando Axios verso l'endpoint dell'API che ci fornisce le foto
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
   const data = response.data; // Alla risposta prendo i dati del campo .data, che è un array di oggetti e ognuno rappresenta un'immagine da inserire nelle card
@@ -10,44 +11,43 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
     col.className = "col";
 
     // ALTERNATIVE FOR EACH
-    
+
     // Alternativa (tentativo): data.map
-        // data.map((picture) => {
-        // const col = document.createElement("div");
-        // col.className = "col";
+    // data.map((picture) => {
+    // const col = document.createElement("div");
+    // col.className = "col";
 
     // Alternativa (tentativo): Ciclo for...of
-      // for (const picture of data) {
-      // const col = document.createElement("div");
-      // col.className = "col"
+    // for (const picture of data) {
+    // const col = document.createElement("div");
+    // col.className = "col"
 
     // Alternativa (tentativo) Ciclo for
-      // for (let i = 0; i < data.length; i++) {
-      //   const picture = data[i];
-      //   const col = document.createElement("div");
-      //   col.className = "col";
-  
+    // for (let i = 0; i < data.length; i++) {
+    //   const picture = data[i];
+    //   const col = document.createElement("div");
+    //   col.className = "col";
 
+    // Inserisco all’interno della colonna una card
+    // Nell’immagine metto l’URL fornito dall’API, il testo sarà recuperato sempre dall'API sfruttando title
+    // Aggiungo direttamente in HTML la card
+        col.innerHTML = `
+      <div class="card h-100" style="position: relative;">
+        <img src="${picture.url}" class="card-img-top" alt="${picture.title}"> 
+        <img src="/img/pin.svg" class="pin-img-top" alt="pin">
+        <div class="card-body">
+          <p class="card-text" id="picture-date">${picture.date || ""}</p>
+          <p class="card-text fw-bold text-uppercase" id="picture-title">${
+            picture.title || ""
+          }</p>
+        </div>
+      </div>
+    `;
 
-  // Inserisco all’interno della colonna una card
-  // Nell’immagine metto l’URL fornito dall’API, il testo sarà recuperato sempre dall'API sfruttando title
-  // Aggiungo direttamente in HTML la card
-    col.innerHTML = `
-  <div class="card h-100" style="position: relative;">
-    <img src="${picture.url}" class="card-img-top" alt="${picture.title}"> 
-    <img src="/img/pin.svg" class="pin-img-top" alt="pin">
-    <div class="card-body">
-      <p class="card-text" id="picture-date">${picture.date || ""}</p>
-      <p class="card-text fw-bold text-uppercase" id="picture-title">${
-        picture.title || ""
-      }</p>
-    </div>
-  </div>
-`;
-    
     // Infine aggiungo la colonna al contenitore, così viene visualizzata in pagina
     container.appendChild(col);
 
+    // OVERLAY
     // Seleziono l'immagine della card
     const imgElement = col.querySelector(".card-img-top");
 
@@ -63,14 +63,13 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
       // Mostro l'overlay
       overlay.classList.remove("d-none");
     });
-  }
-);
-
+  });
 });
 
-// Funzionalità di chiusura overlay
-document.getElementById("close-overlay").addEventListener("click", () => {
-  document.getElementById("custom-overlay").classList.add("d-none");
-});
+    // CHIUSURA OVERLAY
+    // Al click, con eventListener aggiungo d-none
+    document.getElementById("close-overlay").addEventListener("click", () => {
+      document.getElementById("custom-overlay").classList.add("d-none");
+    });
 
 // })
